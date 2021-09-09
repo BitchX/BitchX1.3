e80c9957c232a10c393976cc8531e45fe0c86aeb
cdedwards <cd.edwards@sasktel.net>
2021-09-09 16:48:48 -0600

    Updated for ArchLinux error with gcc

--- source/modules.c
+++ source/modules.c
@@ -83,7 +83,7 @@ extern int (*dcc_output_func) (int, int, char *, int);
 extern int (*dcc_input_func)  (int, int, char *, int, int);
 extern int (*dcc_close_func) (int, unsigned long, int);
 
-int (*serv_open_func) (int, unsigned long, int);
+extern int (*serv_open_func) (int, unsigned long, int);
 extern int (*serv_output_func) (int, int, char *, int);
 extern int (*serv_input_func)  (int, char *, int, int, int);
 extern int (*serv_close_func) (int, unsigned long, int);
--- source/numbers.c
+++ source/numbers.c
@@ -64,11 +64,12 @@ void	show_server_map		(void);
 int	stats_k_grep		(char **);
 void	who_handlekill		(char *, char *, char *);
 void	handle_tracekill	(int, char *, char *, char *);
-int	no_hook_notify;
 extern  AJoinList *ajoin_list;
 void	remove_from_server_list (int);
 
 char	*thing_ansi = NULL;
+
+
 
 /*
  * numeric_banner: This returns in a static string of either "xxx" where
